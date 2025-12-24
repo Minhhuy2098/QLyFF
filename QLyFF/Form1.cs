387ae8f04d083db39f66afd96d6c79c4eeedd7a6
Minhhuy2098 <0023413007@student.dthu.edu.vn>
2025-12-24 20:12:11 +0700

    Update UI logic in Form1

--- QLyFF/Form1.cs
+++ QLyFF/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace QLyFF
@@ -18,6 +12,11 @@ namespace QLyFF
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
